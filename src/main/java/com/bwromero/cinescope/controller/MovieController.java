839b7e439548e6ff3d2c416397c0df38c4686d3b
Bryan Romero <bryan.romerodev@gmail.com>
2026-01-11 22:04:05 +0100

    fix: Added @jsonalias to map in and out same field names for MovieDto

--- src/main/java/com/bwromero/cinescope/controller/MovieController.java
+++ src/main/java/com/bwromero/cinescope/controller/MovieController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/movies")
+@CrossOrigin(origins = "http://localhost:4200")  // Add this!
 public class MovieController {
 
     private final MovieService movieService;
--- src/main/java/com/bwromero/cinescope/dto/MovieDto.java
+++ src/main/java/com/bwromero/cinescope/dto/MovieDto.java
@@ -1,12 +1,13 @@
 package com.bwromero.cinescope.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record MovieDto(
     Long id,
     String title,
-    @JsonProperty("overview") String synopsis,
-    @JsonProperty("poster_path") String posterPath,
-    @JsonProperty("release_date") String releaseDate,
-    @JsonProperty("vote_average") Double rating
+    @JsonAlias("overview") String synopsis,
+    @JsonAlias("poster_path") String posterPath,
+    @JsonAlias("release_date") String releaseDate,
+    @JsonAlias("vote_average") Double rating
 ) {}
